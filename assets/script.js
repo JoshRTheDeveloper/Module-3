3514954cd5437d7ec474889d6c8a06eae80cb44e
Josh Roth <joshroth@Joshuas-MBP.tx.rr.com>
2023-12-21 23:34:10 -0600

    initial Commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 12;
+var password = "";
+
+function generatePassword() {
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+   }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
